6f20035289f3e21511c76879cc443b9d2df324a8
StudyCodeEr <144524071+StudyCodeEr@users.noreply.github.com>
2026-09-01 18:17:54 +0700

    fix bug map

--- Assets/Codes/Reposition.cs
+++ Assets/Codes/Reposition.cs
@@ -9,7 +9,7 @@ public class Reposition : MonoBehaviour
         if (!collision.CompareTag("Area"))
             return;
 
-        Vector3 playerPos = GameManager.instance.transform.position;
+        Vector3 playerPos = GameManager.instance.player.transform.position;
         Vector3 myPos = transform.position;
         float diffX = Mathf.Abs(playerPos.x - myPos.x);
         float diffY = Mathf.Abs(playerPos.y - myPos.y);
