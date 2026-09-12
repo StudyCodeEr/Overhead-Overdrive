0bf552ae110797e249bc43a95e0c641409756851
nguyenviethoa1A <nguyenviethoa2903@gmail.com>
2026-09-12 23:49:26 +0700

    dietquai

--- Assets/Codes/GameManager.cs
+++ Assets/Codes/GameManager.cs
@@ -5,11 +5,41 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    [Header("# Game Control")]
+    public float gameTime;
+    public float maxGameTime = 2 * 10f;
+    [Header("# Player Infor")]
+    public int level;
+    public int kill;
+    public int exp;
+    public int[] nextExp = { 3, 5, 10, 100, 150, 200, 280, 360, 450, 600 };
+    [Header("# Game Object")]
     public PoolManager pool;
     public Player player;
 
     void Awake()
     {
         instance = this;
+    }
+
+    void Update()
+    {
+        gameTime += Time.deltaTime;
+
+        if (gameTime > maxGameTime)
+        {
+            gameTime = maxGameTime;
+        }
+
+    }
+    public void GetExp()
+    {
+        exp++;
+
+        if (exp == nextExp[level])
+        {
+            level++;
+            exp = 0;
+        }
     }
 }
