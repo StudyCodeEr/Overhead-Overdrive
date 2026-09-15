bca874015f59c873cb594c9ec61c75e5e5218c0d
StudyCodeEr <144524071+StudyCodeEr@users.noreply.github.com>
2026-09-15 11:13:46 +0700

    Add nang cap weapon

--- Assets/Codes/Player.cs
+++ Assets/Codes/Player.cs
@@ -22,8 +22,19 @@ public class Player : MonoBehaviour
         hands = GetComponentsInChildren<Hand>(true);
     }
 
+    void Update()
+    {
+        if (!GameManager.instance.isLive)
+            return;
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
     void FixedUpdate()
     {
+        if (!GameManager.instance.isLive)
+            return;
+
         Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
         // MovePosition
         rigid.MovePosition(rigid.position + nextVec);
@@ -36,6 +47,9 @@ public class Player : MonoBehaviour
 
     void LateUpdate()
     {
+        if (!GameManager.instance.isLive)
+            return;
+
         anim.SetFloat("Speed", inputVec.magnitude);
 
         if (inputVec.x != 0) {
